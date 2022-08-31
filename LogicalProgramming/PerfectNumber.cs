5b62b80bdb1393cb699264146401d1be51632e58
navitha <navithareddy112@gmail.com>
2022-08-31 21:21:53 +0530

    [navitha] 2.PerfectNumber

---
+++ LogicalProgramming/PerfectNumber.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgramming
+{
+    internal class PerfectNumber
+    {
+        public static void NumberPerfect()
+        {
+            int num, res = 0, n;
+            Console.Write(" enter the number ");
+            num = int.Parse(Console.ReadLine());
+            n = num;
+            for (int a = 1; a < num; a++)
+            {
+                if (num % a == 0)
+                {
+                    res = res + a;
+                }
+            }
+            if (res == n)
+            {
+                Console.WriteLine("\n Entered number is a perfect number");
+            }
+            else
+            {
+                Console.WriteLine("\n Entered number is not a perfect number");
+            }
+        }
+        
+    }
+}
--- LogicalProgramming/Program.cs
+++ LogicalProgramming/Program.cs
@@ -5,13 +5,16 @@
         static void Main(string[] args)
         {
             //Console.WriteLine("Hello, World!");
-            Console.WriteLine("choose option 1.Fibonacci");
+            Console.WriteLine("choose option 1.Fibonacci \t 2.PerfectNumber");
             int option = Convert.ToInt32(Console.ReadLine());
             switch(option)
             {
                 case 1:
                     Fibonacci.FibonacciNumbers(9);
                     break;
+                case 2:
+                    PerfectNumber.NumberPerfect();
+                        break;
             }
         }
     }
