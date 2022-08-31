4b79c5cb0fc7415c5c8f79c78c2807845be5033a
navitha <navithareddy112@gmail.com>
2022-08-31 20:51:50 +0530

    [navitha] 1.Fibonacci

---
+++ LogicalProgramming/Fibonacci.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgramming
+{
+    internal class Fibonacci
+    {
+        public static void FibonacciNumbers(int len)
+        {
+            int num1 = 0, num2 = 1, num3 = 0;
+            Console.WriteLine("{0} {1} " , num1, num2);
+            for(int i = 0; i < 10 ; i++)
+            {
+                num3 = num1 + num2;
+                Console.WriteLine("{0}",num3);
+                num1 = num2;
+                num2 = num3;
+            }
+            
+        }
+    }
+}
---
+++ LogicalProgramming/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace LogicalProgramming
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello, World!");
+            Console.WriteLine("choose option 1.Fibonacci");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch(option)
+            {
+                case 1:
+                    Fibonacci.FibonacciNumbers(9);
+                    break;
+            }
+        }
+    }
+}
