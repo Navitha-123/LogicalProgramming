3a49bc30b548c473ab2e44e1d0fbe927b2971b22
navitha <navithareddy112@gmail.com>
2023-01-01 20:35:42 +0530

    [navitha] UC6StopWatch

--- LogicalProgramming/Program.cs
+++ LogicalProgramming/Program.cs
@@ -5,7 +5,7 @@
         static void Main(string[] args)
         {
             //Console.WriteLine("Hello, World!");
-            Console.WriteLine("choose option 1.Fibonacci \t 2.PerfectNumber \t 3.Primenumber \t 4.Reversenumber \t 5. Dayofweek \t 6.Temperature \t 7.MonthlyPayment \t 8.Squareroot  \t 9. Binaryvalue \t 10.BinarySwap \t 11.Coupnnumber");
+            Console.WriteLine("choose option 1.Fibonacci \t 2.PerfectNumber \t 3.Primenumber \t 4.Reversenumber \t 5. Dayofweek \t 6.Temperature \t 7.MonthlyPayment \t 8.Squareroot  \t 9. Binaryvalue \t 10.BinarySwap \t 11.Coupnnumber \t 12.stopwatch");
             int option = Convert.ToInt32(Console.ReadLine()); 
             switch(option)
             {
@@ -41,6 +41,9 @@
                     break;
                 case 11:
                     UC5Coupn.Coupnnumber();
+                    break;
+                case 12:
+                    UC6StopWatch.Checkstopwatch();
                     break;
 
             }
---
+++ LogicalProgramming/UC6StopWatch.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgramming
+{
+    public class UC6StopWatch
+    {
+        public static void Checkstopwatch()
+        {
+            Stopwatch stopwatch = new Stopwatch();
+        validation1:
+            Console.WriteLine("to start watch enter S");
+            char start = Convert.ToChar(Console.ReadLine());
+
+            stopwatch.Start();
+            if(start == 's' || start == 'S')
+            {
+                validation2:
+                Console.WriteLine("To stop watch enter Q");
+                char end = Convert.ToChar(Console.ReadLine());  
+                if(end =='q' || end == 'Q')
+                {
+                    stopwatch.Stop();
+                    Console.WriteLine("stopwatch stopped,the time:" +stopwatch.Elapsed);
+                }
+                else
+                {
+                    Console.WriteLine("wrong input");
+                    goto validation2;
+                }
+            }
+            else
+            {
+                stopwatch.Stop();
+                Console.WriteLine("enter corrct input");
+                goto validation1;
+            }
+        }
+    }
+}
